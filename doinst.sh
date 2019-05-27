d9317e422c73b1c255240eedc26aa75e54774e35
flats <flats@kde5.net>
2019-05-27 10:07:07 -0500

    make pkgtool friendly

--- doinst.sh
+++ doinst.sh
@@ -1,11 +1,4 @@
-if [ -f /etc/modprobe.d/nvidia-disable-nouveau.conf ]; then
-	echo /etc/modprobe.d/nvidia-disable-nouveau.conf found
-
-else
-echo blacklist nouveau >> /etc/modprobe.d/nvidia-disable-nouveau.conf
-echo options nouveau modeset=0 >> /etc/modprobe.d/nvidia-disable-nouveau.conf
-fi
-sed -i 's|exec /usr/sbin/nvidia-update||g' /etc/rc.d/rc.local
-echo exec /usr/sbin/nvidia-update >> /etc/rc.d/rc.local
+mv /etc/rc.d/rc.local /etc/rc.d/rc.local.orig
+mv /etc/rc.d/rc.local.new /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 echo you must reboot to install NVIDIA Latest Driver
