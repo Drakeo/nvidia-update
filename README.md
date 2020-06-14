12c741e3e6d411f76c173abf5d9779ed4d614c07
drakeo <flats_fixed@flatsfixedbicycles.com>
2020-06-14 09:37:31 -0500

    have it rebuild at reboot time

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This script will download and install NVida driver
 after you install the package then reboot.
 then anytime after you upgrade the linux-kernel-modules linux-kernel
 script to install NVIDIA latest and create a Slackware package
+
+This script will install durring reboot or shutdown
+At this point you need a working internet durring boot or reboot.  
--- doinst.sh
+++ doinst.sh
@@ -12,4 +12,29 @@ fi
 mv /etc/rc.d/rc.local /etc/rc.d/rc.local.orig
 mv /etc/rc.d/rc.local.new /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
+
+
+if [ -f  /etc/rc.d/rc.local_shutdown ]; then
+ cat /etc/rc.d/rc.local_shutdown > /etc/rc.d/rc.local_shutdown.new
+else 
+echo "#!/bin/sh
+#
+# /etc/rc.d/rc.local_shutdown :  Local system initialization script.  
+exec /usr/sbin/nvidia-update " > /etc/rc.d/rc.local_shutdown
+echo "#!/bin/sh
+#
+# /etc/rc.d/rc.local_shutdown :  Local system initialization script.  
+exec /usr/sbin/nvidia-update " > /etc/rc.d/rc.local_shutdown.new
+fi
+LFNVD=$(cat /etc/rc.d/rc.local_shutdown | grep nvidia)
+if [ "$LFNVD" = "exec /usr/sbin/nvidia-update" ]; then
+  sed -i 's|exec /usr/sbin/nvidia-update||g' /etc/rc.d/rc.local_shutdown.new
+  echo exec /usr/sbin/nvidia-update >> /etc/rc.d/rc.local_shutdown.new
+  else
+  echo exec /usr/sbin/nvidia-update >> /etc/rc.d/rc.local_shutdown.new 
+fi
+
+mv /etc/rc.d/rc.local_shutdown  /etc/rc.d/rc.local_shutdown.orig
+mv /etc/rc.d/rc.local_shutdown.new /etc/rc.d/rc.local_shutdown
+chmod +x /etc/rc.d/rc.local_shutdown
 echo you must reboot to install NVIDIA Latest Driver
