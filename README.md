b3f280778b17c86d2b6c82514f512e6298f0f09b
drakeo <flats_fixed@flatsfixedbicycles.com>
2020-06-28 00:43:40 -0500

    use our own rc.nvidia_update

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ script to install NVIDIA latest and create a Slackware package
 
 This script will install durring reboot or shutdown
 At this point you need a working internet durring boot or reboot.  
+
+when Pat upgrades /etc/rc.d/rc.M you must reinstall.
--- doinst.sh
+++ doinst.sh
@@ -1,40 +1,20 @@
-# need to just add a line to rc.local 
-#keep all of the users previous entries
-cat /etc/rc.d/rc.local > /etc/rc.d/rc.local.new
-# keep from adding double entry for reinstall or upgrades
-FNVD=$(cat /etc/rc.d/rc.local.new | grep nvidia)
-if [ "$FNVD" = "exec /usr/sbin/nvidia-update" ]; then
-sed -i 's|exec /usr/sbin/nvidia-update||g' /etc/rc.d/rc.local.new
-echo exec /usr/sbin/nvidia-update >> /etc/rc.d/rc.local.new
+# Backup rc.M to rc.M.orig
+FNVD=$(cat /etc/rc.d/rc.M | grep Start-nvidia )
+if [ "$FNVD" = "# Start-nvidia setup procedure." ]; then
+   echo installing
+   rm /etc/rc.d/rc.M.new
 else
-echo exec /usr/sbin/nvidia-update >> /etc/rc.d/rc.local.new
-fi
-mv /etc/rc.d/rc.local /etc/rc.d/rc.local.orig
-mv /etc/rc.d/rc.local.new /etc/rc.d/rc.local
-chmod +x /etc/rc.d/rc.local
-
-
-if [ -f  /etc/rc.d/rc.local_shutdown ]; then
- cat /etc/rc.d/rc.local_shutdown > /etc/rc.d/rc.local_shutdown.new
-else 
-echo "#!/bin/sh
-#
-# /etc/rc.d/rc.local_shutdown :  Local system initialization script.  
-exec /usr/sbin/nvidia-update " > /etc/rc.d/rc.local_shutdown
-echo "#!/bin/sh
-#
-# /etc/rc.d/rc.local_shutdown :  Local system initialization script.  
-exec /usr/sbin/nvidia-update " > /etc/rc.d/rc.local_shutdown.new
-fi
-LFNVD=$(cat /etc/rc.d/rc.local_shutdown | grep nvidia)
-if [ "$LFNVD" = "exec /usr/sbin/nvidia-update" ]; then
-  sed -i 's|exec /usr/sbin/nvidia-update||g' /etc/rc.d/rc.local_shutdown.new
-  echo exec /usr/sbin/nvidia-update >> /etc/rc.d/rc.local_shutdown.new
-  else
-  echo exec /usr/sbin/nvidia-update >> /etc/rc.d/rc.local_shutdown.new 
+   mv /etc/rc.d/rc.M  /etc/rc.d/rc.M.orig
+   mv /etc/rc.d/rc.M.new /etc/rc.d/rc.M
+chmod +x /etc/rc.d/rc.M
 fi
 
-mv /etc/rc.d/rc.local_shutdown  /etc/rc.d/rc.local_shutdown.orig
-mv /etc/rc.d/rc.local_shutdown.new /etc/rc.d/rc.local_shutdown
-chmod +x /etc/rc.d/rc.local_shutdown
-echo you must reboot to install NVIDIA Latest Driver
+if [ -f  /etc/rc.d/rc.nvidia_update ]; then
+   mv /etc/rc.d/rc.nvidia_update /etc/rc.d/rc.nvidia_update.orig 
+   mv /etc/rc.d/rc.nvidia_update.new /etc/rc.d/rc.nvidia_update
+   chmod +x /etc/rc.d/rc.nvidia_update
+else
+   mv /etc/rc.d/rc.nvidia_update.new /etc/rc.d/rc.nvidia_update
+   chmod +x /etc/rc.d/rc.nvidia_update   
+ fi
+echo reboot your machine to install nvidia drivers
